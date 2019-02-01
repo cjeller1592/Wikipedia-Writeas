93f62e06a3509804432e60b779e575716b996f31
CJ Eller <45696734+cjeller1592@users.noreply.github.com>
2019-02-01 15:52:00 -0500

    Instructions on how to retrieve token

--- settings.py
+++ settings.py
@@ -1,1 +1,6 @@
-token = 'c6e36c0b-c65e-42b5-4770-36192bd0e5ca'
+token = '00000000-0000-0000-0000-000000000000000'
+
+#Place your token here! 
+#Retrieve it through making a log in request through the API: https://developers.write.as/docs/api/#authenticate-a-user
+#Or through the API client library for Python: https://github.com/cjeller1592/Writeas-API
+
